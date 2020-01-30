52cbd4f2dd84f0cef9532602cf594f512c1892f6
gabriel <cg.marrero@cfodin.qc.ca>
2020-01-29 20:05:53 -0500

    labo 2 termine

--- Labos/laboratoire02/index.php
+++ Labos/laboratoire02/index.php
@@ -160,6 +160,7 @@ for ($i = 0; $i <= 13; $i++) {
     echo moisEnLiterral($i) . ", " . moisEnLiterral($i, false) . ", " . moisEnLiterral($i, true) . "<br/>";
 }
 echo "<br/>";
+
 //Exercice 1 : Création de la fonction joursemaineEnLiterral
 
 
@@ -199,20 +200,109 @@ function joursemaineEnLiterral($intNoJour, $binMajuscule = false) {
 
         default:
             break;
-        
-       
     }
-     if($binMajuscule)
-            $strJour = ucfirst ($strJour);
-     
-     return $strJour;
-    
+    if ($binMajuscule)
+        $strJour = ucfirst($strJour);
+
+    return $strJour;
 }
 
 //test de la fonction joursemaineEnLiterral
-for($i = 0 ; $i<=8;$i++){
-    echo joursemaineEnLiterral($i) . ", " . joursemaineEnLiterral($i,false) . ", " . joursemaineEnLiterral($i, true) . "<br/>";
+for ($i = 0; $i <= 8; $i++) {
+    echo joursemaineEnLiterral($i) . ", " . joursemaineEnLiterral($i, false) . ", " . joursemaineEnLiterral($i, true) . "<br/>";
 }
+
+//Exercice 2 : Creation de la fonction er
+/* FONCTION 
+  |-----------------------------------------------------------------------------------|
+  |er (2020-01-29)
+  | Scénarios :er($intEntier)              => Nombre passer en argument 1 ou autre
+  |            er($intJour,$binExposant)   => En focntion de $binExposant
+  |-----------------------------------------------------------------------------------|
+ */
+function er($intEntier, $binExposant = true) {
+
+
+    return $intEntier . ($intEntier == 1 ? ($binExposant ? "<sup>er</sup>" : "er") : "");
+}
+
+//test de la fonction er
+
+echo "<br/>";
+echo er(1) . ", ";
+echo er(1, false) . ", ";
+echo er(1, true) . "<br/>";
+echo er(31) . "<br/><br/>";
+
+//Exercice 3 : Creation de la fonction extraitJSJJMMAAAA
+/* FONCTION 
+  |--------------------------------------------------------------------------------------|
+  |extraitJSJJMMAAAA (2020-01-29)
+  | Scénarios :extraitJSJJMMAAAA(&$intJourSemaine,&$intJour,&$intMois,&$intAnnee)
+  |           extraitJSJJMMAAAA(&$intJourSemaine,&$intJour,&$intMois,&$intAnnee,$strDate)
+  |--------------------------------------------------------------------------------------|
+ */
+
+function extraitJSJJMMAAAA(&$intJourSemaine, &$intJour, &$intMois, &$intAnnee) {
+    //Par defaut, l'extraction s'effectue a partir de la date courante;
+    //Autrement dit, elle s'effectue a partir du 5e argument specifie a l'appel
+    if (func_num_args() == 4) {
+        //Recuperation de la date courante
+        $strDate = date("d-m-Y");
+        //Recuperation du jour de la semaine
+        $intJourSemaine = date("N")-1;
+          $intJour = intval(substr($strDate, 0, 2)) -1;
+    } else {
+        //Recuperation du 5e argument
+        $strDate = func_get_arg(4);
+        //Recuperation du jour de la semaine apres convertit la date passee
+        //sous forme d'une chaine en timestamp
+        $intJourSemaine = date("N", strtotime($strDate)) ;
+          $intJour = intval(substr($strDate, 0, 2)) ;
+    }
+  
+    $intMois = intval(substr($strDate, 3, 2));
+    $intAnnee = intval(substr($strDate, 6, 4));
+}
+
+//test de la fonction extraitJSJJMMAAAA
+
+$intJour6;
+$intMois6;
+$intAnnee6;
+$intJourSemaine6;
+//Extraction a partir de la date courante
+echo "<br/>";
+extraitJSJJMMAAAA($intJourSemaine6, $intJour6, $intMois6, $intAnnee6);
+echo "Nous sommes le " . joursemaineEnLiterral($intJourSemaine6) . " " . er($intJour6) . " " . moisEnLiterral($intMois6) . "  $intAnnee6 . <br/><br/>";
+
+//2e test de la fonction extraitJSJJMMAAAA
+
+$intJour7;
+$intMois7;
+$intAnnee7;
+$intJourSemaine7;
+
+//Extraction a partir de la date specifiee
+for($i = 1 ; $i <= 31;$i++){
+    extraitJSJJMMAAAA($intJourSemaine7, $intJour7, $intMois7, $intAnnee7,($i < 10 ? "0" : "") . "$i-01-2020");
+    echo "Nous sommes le " . joursemaineEnLiterral($intJourSemaine7). " ".er($intJour7)." ".moisEnLiterral($intMois7)." $intAnnee7.<br/>";
+}
+    
+
+
+
+
+/*echo "Resultat de la fonction extraitJSJJMMAAAA<br/>";
+echo "jour : " . $intJour6 . "<br/>";
+echo "mois : " . $intMois6 . "<br/>";
+echo "annee : " . $intAnnee6 . "<br/>";
+echo "Jour de la semaine : " . $intJourSemaine6 . "<br/>";*/
+
+echo "Jour de la semaine : " . $intJourSemaine6 . "<br/>";
+//echo date("N");
+//echo joursemaineEnLiterral(3);
+//echo moisEnLiterral(1)
 ?>
 
 
